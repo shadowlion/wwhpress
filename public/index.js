6ecddce673743cdda12f4a8a1bb725e58f6fa27c
Justin Chiou <chiou.kai@gmail.com>
2021-03-19 14:18:34 -0400

    init commit

---
+++ public/index.js
@@ -0,0 +1,32 @@
+const stripe = Stripe(
+  "pk_test_51ISoQ4FerEI1oMqtSjXSRYVnPONSpRYBkuVZ7ROLfRojsQqCmM0rYqlaXGaZOs6jtVHAOo9EDZobN3nyWRl9Fdtc008mvdhP3u"
+);
+
+const checkoutButton = document.querySelector("#checkout");
+checkoutButton.addEventListener("click", async () => {
+  console.log("checking out...");
+  try {
+    const result = await stripe.redirectToCheckout({
+      lineItems: [
+        {
+          price: "price_1ISq1IFerEI1oMqtTFD7VnRr",
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      successUrl: "https://example.com/success",
+      cancelUrl: "https://example.com/cancel",
+      shippingAddressCollection: {
+        allowedCountries: ["US", "CA"],
+      },
+      submitType: "book",
+    });
+
+    if (result.error) {
+      const displayError = document.querySelector("#error");
+      displayError.textContent = result.error.message;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
