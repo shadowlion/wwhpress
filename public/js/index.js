b006fd312c444a35ff88f73d86e42eb48ad259cb
Justin Chiou <chiou.kai@gmail.com>
2021-04-24 19:04:41 -0400

    refactor: move product config out of frontend code

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,15 @@
-const stripe = Stripe(
-  "pk_test_51ISoQ4FerEI1oMqtSjXSRYVnPONSpRYBkuVZ7ROLfRojsQqCmM0rYqlaXGaZOs6jtVHAOo9EDZobN3nyWRl9Fdtc008mvdhP3u"
-);
-
 const checkoutButton = document.querySelector("#cc-checkout");
+
 checkoutButton.addEventListener("click", async () => {
   try {
-    const result = await stripe.redirectToCheckout({
-      lineItems: [
-        {
-          price: "price_1ITs72FerEI1oMqtrfOkOl36",
-          quantity: 1,
-        },
-        {
-          price: "price_1IWoxcFerEI1oMqthr0SLg0d",
-          quantity: 1,
-        }
-      ],
-      mode: "payment",
-      successUrl: "https://wwhpress.netlify.app/success",
-      cancelUrl: "https://wwhpress.netlify.app/cancel",
-      shippingAddressCollection: {
-        allowedCountries: ["US", "CA"],
-      },
-      submitType: "book",
-    });
+    const url = "/.netlify/functions/create-checkout";
+    const response = await fetch(url);
+    const { sessionId, publishableKey } = await response.json();
+    const stripe = Stripe(publishableKey);
+    const { error } = await stripe.redirectToCheckout({ sessionId });
 
-    if (result.error) {
-      const displayError = document.querySelector("#error");
-      displayError.textContent = result.error.message;
+    if (error) {
+      console.error(error);
     }
   } catch (error) {
     console.error(error);
